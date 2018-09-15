ca0691b75d035a2185ba5e8d72d4f91203694a9b
Tom Davidson <tom@davidson.me.uk>
2018-09-15 13:15:34 +0100

    official urls

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ if [ -d './build' ]; then
 fi
 mkdir build
 
-# sslyze
+# sslyze: https://github.com/nabla-c0d3/sslyze
 docker rmi nabla-c0d3/sslyze
 git clone git@github.com:nabla-c0d3/sslyze.git build/sslyze
 rm -Rf build/sslyze/.git
 docker build --rm -f Dockerfile/sslyze -t nabla-c0d3/sslyze ./build/sslyze
 
-# arachne (mac)
+# arachne (mac): http://www.arachni-scanner.com/
 wget https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-darwin-x86_64.tar.gz -O build/arachni.tar.gz
 cd build && tar -xzf arachni.tar.gz && rm -f arachni.tar.gz && cd ../
 
-# sqlmap
+# sqlmap: http://sqlmap.org/
 docker pull paoloo/sqlmap
