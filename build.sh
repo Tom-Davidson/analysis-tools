48781531414a8e3a88c45a7f091683ee661cb499
Tom Davidson <tom@davidson.me.uk>
2018-09-15 13:04:06 +0100

    removing .git dir from sslyze to reduce image size

--- build.sh
+++ build.sh
@@ -9,4 +9,8 @@ mkdir build
 # sslyze
 docker rmi nabla-c0d3/sslyze
 git clone git@github.com:nabla-c0d3/sslyze.git build/sslyze
-docker build -f Dockerfile/sslyze -t nabla-c0d3/sslyze ./build/sslyze
+docker build --rm -f Dockerfile/sslyze -t nabla-c0d3/sslyze ./build/sslyze
+
+# arachne (mac)
+wget https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-darwin-x86_64.tar.gz -O build/arachni.tar.gz
+cd build && tar -xzf arachni.tar.gz && rm -f arachni.tar.gz && cd ../
