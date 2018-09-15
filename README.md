3b2ce7109a2d16b199de910b8646d61d46c8e2e7
Tom Davidson <tom@davidson.me.uk>
2018-09-15 13:12:19 +0100

    sqlmap

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ Build a set of docker images for analysis tools so they can be easily run.
 ## sslyze
 `docker run --rm -it nabla-c0d3/sslyze --regular www.github.com:443 > sslyze.txt`
 
+## sqlmap
+`docker run --rm -it -v /tmp/sqlmap:/root/.sqlmap/ paoloo/sqlmap --url www.example.com/?id=1`
+and the report will be found at /tmp/sqlmap
+
 ## TODO
  - build should version docker containers
--- build.sh
+++ build.sh
@@ -9,8 +9,12 @@ mkdir build
 # sslyze
 docker rmi nabla-c0d3/sslyze
 git clone git@github.com:nabla-c0d3/sslyze.git build/sslyze
+rm -Rf build/sslyze/.git
 docker build --rm -f Dockerfile/sslyze -t nabla-c0d3/sslyze ./build/sslyze
 
 # arachne (mac)
 wget https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-darwin-x86_64.tar.gz -O build/arachni.tar.gz
 cd build && tar -xzf arachni.tar.gz && rm -f arachni.tar.gz && cd ../
+
+# sqlmap
+docker pull paoloo/sqlmap
